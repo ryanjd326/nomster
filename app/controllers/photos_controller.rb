49f671df09d0236c056162d51cc4f67a8b54cd6b
Ryan Dickinson <ryanjd326@gmail.com>
2019-10-06 18:20:37 +0000

    reverted photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user!
 
-  def create(photo)
+  def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
